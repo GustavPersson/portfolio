0b7c0eb890acba15a5229c0addf53208f860b9d0
Gustav Persson <gustav.persson@modcam.io>
2017-02-26 21:26:37 +0100

    Grejer till sidan, minns inte vad

--- scripts/script.js
+++ scripts/script.js
@@ -19,7 +19,6 @@ $(document).ready(function() {
     });
   });
 
-
   $('.preview').click(function(event) {
     event.stopPropagation();
     event.preventDefault();
@@ -31,11 +30,34 @@ $(document).ready(function() {
     $('.content').slideDown(600).addClass('displayed');
     $('.projects.section').addClass('displayed');
     $('.close').show();
+
+    $.scrollTo($('.projects'), 100, {
+      interrupt: false
+    });
+
+    var projectElement = $('.projects .right'),
+        height = projectElement.height();
+
+
+    if(window.outerWidth > 768) {          
+      projectElement.bind('mousewheel', function(e, d) {
+        var scrollHeight = $(e.currentTarget)[0].scrollHeight;
+        if((this.scrollTop === (scrollHeight - height) && d < 0) || (this.scrollTop === 0 && d > 0)) {
+          e.preventDefault();
+        }
+      });
+    }
+
   });
 
   $('.close').click(function(event) {
     event.stopPropagation();
     event.preventDefault();
+
+    var projectElement = $('.projects .right');
+
+    projectElement.unbind('mousewheel');
+
     $('.project-container.displayed').removeClass('displayed');
     $('.content.displayed').slideUp(400).removeClass('displayed');
     $('.projects.section').removeClass('displayed');
